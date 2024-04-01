de93005ffe5ce654b5e3cd10ab7b5b4c6dd1f0eb
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2024-04-02 02:25:00 +0800

    Added kernel functions for both C and ASM

--- lbyarch-mp2/asmfunc.asm
+++ lbyarch-mp2/asmfunc.asm
@@ -1,6 +1,3 @@
-section .data
-msg db "Hello World",13,10,0
-
 section .text
 bits 64
 default rel ; to handle address relocation
@@ -8,9 +5,28 @@ default rel ; to handle address relocation
 global asm_dot_product
 extern printf
 
+;Performs a dot product between 2 vectors (x86_64 Assembly Version)
+; Params:
+; RCX - n
+; RDX - vectorA
+; R8 - vectorB
+; Returns XMM0 (sdot)
 asm_dot_product:
-	sub rsp, 8*5 ; caller
-	lea rcx, [msg]
-	call printf
-	add rsp, 8*5
-	ret
+	PUSH RBX ; initialize counter (i)
+	XOR RBX, RBX ; reset counter to 0
+	XORPS XMM0, XMM0 ; reset reg
+
+	SUMMATION:
+		MOVSS XMM1, dword[RDX + RBX * 4] ; Initialize with multiplicand
+		MULSS XMM1, dword[R8 + RBX * 4] ; Multiply to multiplier
+		ADDSS XMM0, XMM1 ; Save product to reg
+
+		INC RBX ; Increment counter
+		CMP RBX, RCX ; If i==n
+		JZ END_SUMMATION ; End loop
+
+		JMP SUMMATION ; Else proceed to next iteration
+
+	END_SUMMATION:
+		POP RBX ; clear counter
+		ret
--- lbyarch-mp2/main.c
+++ lbyarch-mp2/main.c
@@ -4,7 +4,41 @@
 
 extern void asm_dot_product();
 
+/*
+* Initializes the 2 vectors with the given data
+*
+* @param n - length of the vector
+* @param vectorA - factor 1
+* @param vectorB - factor 2
+*
+*/
+void initializeVectors(int n, float* vectorA, float* vectorB) {
+	for (int i = 0; i < n; i++) {
+		vectorA[i] = (float)i;
+		vectorB[i] = 0.5f;
+	}
+}
+
 int main(int argc, char* argv[]) {
 	asm_dot_product();
 	return 0;
+}
+
+
+/*
+* Performs a dot product between 2 vectors
+* 
+* @param n - length of the vector
+* @param vectorA - factor 1
+* @param vectorB - factor 2
+* 
+* @return sdot - result of the dot product
+*/
+float c_dot_product(int n, float* vectorA, float* vectorB) {
+	float sdot = 0.0; // Initialize output var
+
+	for (int i = 0; i < n; i++) {
+		sdot += vectorA[i] * vectorB[i];
+	}
+	return sdot;
 }
